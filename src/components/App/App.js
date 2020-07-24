903e568f4b8f2589f1650b25d0fdb21e85127956
Mohamed iliasTamedi <tam.ilias22000@gmail.com>
2020-07-24 15:18:26 +0200

    client inscription

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,29 @@
  */
 
 /*Module imports*/
-import React from "react";
+import React, { Component } from "react";
+import Inscription from "../Inscription/Inscription";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Connexion from "../Connexion/Connexion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+/*Main app component*/
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/Inscription" exact component={Inscription} />
+            <Route path="/Connexion" exact component={Connexion} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
 export default App;
--- src/components/Inscription/Inscription.js
+++ src/components/Inscription/Inscription.js
@@ -1,14 +1,73 @@
-import React, { Component, useReducer } from "react";
+import React, { Component } from "react";
 import "./inscription.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import FormControl from "react-bootstrap/FormControl";
-import { Link } from "react-router-dom";
+/*import FormControl from "react-bootstrap/FormControl";*/
+/*import { Link } from "react-router-dom";*/
 
 class Inscription extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      lastname: null,
+      firstname: null,
+      email: null,
+      password: null,
+      gender: null,
+      age: null,
+      adress: null,
+      phone: null,
+    };
   }
+
+  /*fonction pour ecrire dans nos input*/
+  change = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value, // identifier Id de l'input = choisir la valeur qui se trouve dans l'input
+    });
+  };
+
+  addNewRegister = (e) => {
+    /*alert(
+      "Votre inscription a bien été prise en compte. Un email de confirmation vous a été envoyé. Merci"
+    );*/
+    e.preventDefault();
+    const data = {
+      lastname: this.state.lastname,
+      firstname: this.state.firstname,
+      email: this.state.email,
+      password: this.state.password,
+      gender: this.state.gender,
+      age: this.state.age,
+      adress: this.state.adress,
+      phone: this.state.phone,
+    };
+
+    const headers = new Headers({
+      "Content-Type": "application/json",
+    });
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: headers,
+    };
+
+    fetch("http://localhost:4000/client/register", options)
+      .then((response) => {
+        return response.json();
+      })
+      .then(
+        (responseObject) => {
+          this.setState({ message: responseObject.message });
+          /*this.props.history.push("/Home"); */ // pour affiher le profil
+        },
+
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
   render() {
     return (
@@ -18,60 +77,96 @@ class Inscription extends Component {
         </div>
         <Form>
           <p className="sous-titre">Données requises </p>
-          <Form.Group controlId="formNom">
+          <Form.Group controlId="lastname">
             <Form.Label>Nom</Form.Label>
-            <Form.Control type="text" placeholder="Nom" name="nom" />
+            <Form.Control
+              type="text"
+              placeholder="Nom"
+              name="lastname"
+              onChange={this.change}
+              value={this.state.lastname}
+            />
           </Form.Group>
-          <Form.Group controlId="formPrenom">
+          <Form.Group controlId="firstname">
             <Form.Label>Prénom</Form.Label>
-            <Form.Control type="text" placeholder="Prénom" name="prenom" />
+            <Form.Control
+              type="text"
+              placeholder="Prénom"
+              name="firstname"
+              onChange={this.change}
+              value={this.state.firstname}
+            />
           </Form.Group>
 
-          <Form.Group controlId="formMail">
+          <Form.Group controlId="email">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="mail" placeholder="Email " name="mail" />
+            <Form.Control
+              type="mail"
+              placeholder="email"
+              name="email"
+              onChange={this.change}
+              value={this.state.email}
+            />
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
-          <Form.Group controlId="formPass">
+          <Form.Group controlId="password">
             <Form.Label>Mot de passe</Form.Label>
             <Form.Control
               type="password"
               placeholder="Mot de passe"
               name="password"
-            />
-          </Form.Group>
-          <Form.Group controlId="formPass2">
-            <Form.Label>Confirmer mot de passe</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Mot de passe"
-              name="passwords"
+              onChange={this.change}
+              value={this.state.password}
             />
           </Form.Group>
         </Form>
 
         <Form>
           <p className="sous-titre">Données facultatives </p>
-          <Form.Group controlId="formVille">
-            <Form.Label>Genre </Form.Label>
+          <Form.Group controlId="gender">
+            <Form.Label> Genre </Form.Label>
             <Form.Control
+              as="select"
               type="text"
               placeholder="Femme; Homme; Autre"
-              name="ville"
+              name="gender"
+              onChange={this.change}
+              value={this.state.gender}
+            >
+              <option>Femme</option>
+              <option>Homme</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="adress">
+            <Form.Label>Ville ou Code Postal</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ville"
+              name="adress"
+              onChange={this.change}
+              value={this.state.adress}
             />
           </Form.Group>
-          <Form.Group controlId="formVille">
-            <Form.Label>Ville ou Code Postal</Form.Label>
-            <Form.Control type="text" placeholder="Ville" name="ville" />
-          </Form.Group>
 
-          <Form.Group controlId="formTel">
+          <Form.Group controlId="phone">
             <Form.Label>Numéro de téléphone</Form.Label>
-            <Form.Control type="text" placeholder="Telephone" name="tel" />
+            <Form.Control
+              type="text"
+              placeholder="Telephone"
+              name="phone"
+              onChange={this.change}
+              value={this.state.phone}
+            />
           </Form.Group>
-          <Form.Group controlId="formPoste">
-            <Form.Label>Poste</Form.Label>
-            <Form.Control type="text" placeholder="Poste occupé" name="poste" />
+          <Form.Group controlId="ages">
+            <Form.Label>Age</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Poste occupé"
+              name="age"
+              onChange={this.change}
+              value={this.state.age}
+            />
             <Form.Text className="text-muted"></Form.Text>
           </Form.Group>
         </Form>
@@ -86,16 +181,17 @@ class Inscription extends Component {
               label="J'accepte que mes données soient utilisés à des fins commerciales."
             />
           </Form.Group>
-          <Link to="/connexion">
-            <Button
-              className="submitButton"
-              variant="primary"
-              block
-              type="submit"
-            >
-              S'inscrire
-            </Button>
-          </Link>
+
+          <Button
+            className="submitButton"
+            variant="primary"
+            block
+            type="submit"
+            onClick={this.addNewRegister}
+          >
+            S'inscrire
+          </Button>
+          <p>{this.state.message}</p>
         </Form>
       </div>
     );
